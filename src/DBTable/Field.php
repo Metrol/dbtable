0b0b6ea4b62fb72dd9a55bd5a3adf9b555b66487
Michael Collette <metrol@metrol.net>
2016-07-08 10:24:23 -0500

    Moves the FieldValue methods into the Field interface
    Every kind of field, without regard to db engine, will need some kind of
    data validation.  This also makes IDE code completion work correctly.

--- src/DBTable/Field.php
+++ src/DBTable/Field.php
@@ -86,4 +86,42 @@ interface Field
      * @return $this
      */
     public function setDefinedType($typeName);
+
+    /**
+     * Tells the field object not to try and get the value to fit if it's
+     * outside the allowed boundaries.  Instead, throw a RangeException for
+     * problems found.
+     *
+     * @param boolean $flag
+     *
+     * @return $this
+     */
+    public function setStrictValues($flag = true);
+
+    /**
+     * The value passed in will be converted to a PHP scalar/array/object
+     * depending on the type of field in use.
+     *
+     * @param mixed $inputValue
+     *
+     * @return mixed
+     *
+     * @throws \RangeException
+     */
+    public function getPHPValue($inputValue);
+
+    /**
+     * The value passed in will be converted to a format ready to be bound
+     * to a SQL engine execute.  Objects and arrays will be converted to their
+     * string representations.
+     *
+     * No quotes or escaping of characters will be performed.
+     *
+     * @param mixed $inputValue
+     *
+     * @return mixed
+     *
+     * @throws \RangeException
+     */
+    public function getSqlBoundValue($inputValue);
 }
